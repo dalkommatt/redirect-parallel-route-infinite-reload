20fd68d9be93ccc6e7d15c410570e352f48caa8b
Matt <mr.matthewc.king@gmail.com>
2024-07-06 12:53:27 -0600

    first commit

---
+++ app/@modal/(.)login/page.tsx
@@ -0,0 +1,7 @@
+export default function ModalLoginPage() {
+  return (
+    <div className="absolute inset-0 flex items-center justify-center bg-black/[.5] dark:bg-white/[.5]">
+      <h1>Login here</h1>;
+    </div>
+  );
+}
